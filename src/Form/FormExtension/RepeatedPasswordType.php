1c61461d6f41094fe4bb596dd4f72ec4cce0ae81
Ininahazwe <yves.cri@gmail.com>
2021-04-21 20:52:22 +0200

    réinitialisation du mot de passe

---
+++ src/Form/FormExtension/RepeatedPasswordType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form\FormExtension;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RepeatedPasswordType extends AbstractType
+{
+    public function getParent(): string
+    {
+        return RepeatedType::class;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'type'              =>PasswordType::class,
+            'invalid_message'   => "Passwords are different ",
+            'required'          => true,
+            'first_options'     => [
+                'label'         => "Password",
+                'label_attr'    => [
+                    'title'     => 'For security reasons, your password must contains...',
+                ],
+                'attr'          => [
+                    'pattern'   => "^(?=.*[a-zà-ÿ])(?=.*[A-ZÀ-Ý])(?=.*[0-9])(?=.*[^a-zà-ÿA-ZÀ-Ý0-9]).{12,}$",
+                    'title'     => "For security reasons, your password must contains",
+                    'maxlength' => 255
+                ]
+            ],
+            'second_options'        => [
+                'label'             => "Confirm password",
+                'label_attr'        => [
+                    'title'         => 'Confirm password'
+                ],
+                'attr'              => [
+                    'pattern'       => "^(?=.*[a-zà-ÿ])(?=.*[A-ZÀ-Ý])(?=.*[0-9])(?=.*[^a-zà-ÿA-ZÀ-Ý0-9]).{12,}$",
+                    'title'         => "Confirm  password",
+                    'maxlength'     => 255
+                ]
+            ],
+        ]);
+    }
+}
