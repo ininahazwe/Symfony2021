44a0baa9fac396739607c4eeb284a53da8d198ae
Ininahazwe <yves.cri@gmail.com>
2021-04-23 19:57:45 +0200

    ajout de fixtures user via une api

---
+++ src/EventSubscriber/UserPasswordEncoderSubscriber.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserPasswordEncoderSubscriber implements EventSubscriber
+{
+    private UserPasswordEncoderInterface $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function getSubscribedEvents(): array
+    {
+        return [
+            Events::prePersist,
+            Events::preUpdate
+        ];
+    }
+    public function prePersist(LifecycleEventArgs $args): void
+    {
+        $this->encodeUserPassword($args);
+    }
+    public function preUpdate(LifecycleEventArgs $args): void
+    {
+        $this->encodeUserPassword($args);
+    }
+    private function encodeUserPassword(LifecycleEventArgs $args): void
+    {
+        $entity = $args->getObject();
+
+        if(!$entity instanceof User){
+            return;
+        }
+        $entity->setPassword($this->encoder->encodePassword($entity, $entity->getPassword()));
+    }
+}
